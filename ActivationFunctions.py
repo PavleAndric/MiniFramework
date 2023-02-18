fixed bugs and  tried to add Softmax

ActivationFunctions.py
@@ -1,4 +1,5 @@
 from Activations import Activation
+from Layer import Layer
 import numpy as np
 
 class Tanh(Activation):
@@ -9,7 +10,6 @@ class Tanh(Activation):
         der_act = lambda input: 1- np.power(np.tanh(input), 2)
         super().__init__(act, der_act)
         
-
 class Sigmoid(Activation):
 
     def __init__(self):
@@ -23,4 +23,18 @@ class ReLU(Activation):
     def __init__(self):
         act = lambda input: np.maximum(input, 0)
         der_act = lambda input: input > 0
+        super().__init__(act, der_act)
+
+class Softmax(Activation):
+
+    def __init__(self):
+
+        def act(input):
+            expo = np.exp(input)
+            act =  expo/ np.sum(expo , axis = 0)
+            return act
+        
+        def der_act(input):
+            pass
+            
         super().__init__(act, der_act)

Activations.py
@@ -9,12 +9,13 @@ class Activation(Layer):
         self.der_activation  = der_activation
 
     def forward(self,input):
+
         self.input  = input 
         return self.activation(input)
 
     def backward(self,i_grad, lr = None):
-
-        ret = np.multiply(i_grad, self.der_activation(self.input))
+        
+        ret = np.multiply(i_grad, self.der_activation(self.input)) #Element wise mul 
         return ret
     
 

HelperFunctions.py
@@ -1,4 +1,4 @@
-### PREDS FOR XOR ONLY
+import numpy as np
 
 def prediction(X, Y, net):
  
@@ -10,8 +10,8 @@ def prediction(X, Y, net):
 
     print(f"predicted {output} , label = {y}")
     
-def forward_pass(net, output):
-
+def forward_pass(net, input):
+    output = input
     for layer in net:
         output = layer.forward(output)
     return output
@@ -20,3 +20,8 @@ def backward_pass(net, gradient, lr):
 
     for layer in reversed(net):
         gradient = layer.backward(gradient, lr)
+
+def hot_encode(Y_true, Y_pred):
+    hot_encoded = np.zeros((Y_pred.size,1))
+    hot_encoded[Y_true[0]][0] = 1
+    return hot_encoded 

Layer.py
@@ -3,14 +3,15 @@ import numpy as np
 class Layer():
 
     def __init__(self, input_neurons, output_neurons):
-                
+        
         self.W = np.random.randn(output_neurons,input_neurons)
         self.B = np.random.randn(output_neurons, 1)
 
     def forward(self, input):
 
         self.X = input  #values of neurons of the current layer
-        return np.dot(self.W, self.X) + self.B #values of neurons of the next layer
+        ret  = np.dot(self.W, self.X) + self.B
+        return  ret #values of neurons of the next layer
     
     def backward(self ,o_grad, learning_rate):
         